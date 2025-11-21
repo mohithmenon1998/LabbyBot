df89d4e0d173077924bc87a4254a02f7604c0441
mohithmenon1998 <mohithmenon1998@gmail.com>
2025-11-21 17:42:11 +0530

    updated main.ipynb

---
+++ src/config.py
@@ -0,0 +1,4 @@
+recursive_split_chunk_size = 1000
+recursive_split_chunk_overlap = 200
+raw_files_path = "E:/AI_Engineer/portfolio/RAG/LabbyBot/data/raw/class_9"
+processed_files_path = "E:/AI_Engineer/portfolio/RAG/LabbyBot/data/processed/chromadb"
